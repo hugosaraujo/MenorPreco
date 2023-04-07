c405cf426412ce38176331c980c3684954800932
Hugo de Araujo <hugo.saraujo@tutanota.com>
2023-04-06 22:51:43 -0300

    Adicionar arquivos de projeto.

---
+++ Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenorPreco
+{
+    internal class Produto
+    {
+        string nome;
+        string nomeFabricante;
+        string codBarras;
+        double valorUnitario;
+        double valorDeCompra;
+        int qtdEstoque; 
+    }
+}
